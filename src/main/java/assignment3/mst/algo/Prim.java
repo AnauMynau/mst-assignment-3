049b4bc96b5b4c3980df9c988a5be93c51528180
inayatulla <241637@astanait.edu.kz>
2025-10-22 22:43:50 +0500

    add Graph class with adjacency list and edges

--- src/main/java/assignment3/mst/algo/Prim.java
+++ src/main/java/assignment3/mst/algo/Prim.java
@@ -1,4 +1,40 @@
 package assignment3.mst.algo;
 
+import assignment3.mst.model.Edge;
+import assignment3.mst.model.Graph;
+import assignment3.mst.model.ResultDTO;
+
+import java.util.*;
+
 public class Prim {
+    public static ResultDTO.AlgoResult run(Graph g ) {
+        long t0 = System.nanoTime();
+        ResultDTO.AlgoResult out = new ResultDTO.AlgoResult();
+        if (g.V == 0) { out.mstCost = 0; out.timeMs = 0; return out; }
+
+        boolean[] vis = new boolean[g.V];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        //int[] = {weight, from, to
+        vis[0] = true;
+        for (Edge e: g.adj.get(0)) {
+            pq.add(new int[]{e.w, e.u, e.v});
+            out.ops.edgeConsidered++;
+        }
+
+        int taken = 0;
+        long total = 0;
+
+        while (!pq.isEmpty() && taken < g.V -1) {
+            int[] cur = pq.poll();
+            out.ops.pqPops++;
+            int w = cur[0], from = cur[1], to = cur[2];
+            if (vis[to]) {
+                continue;
+            }
+            vis[to] = true;
+
+        }
+
+        return out;
+    }
 }
--- src/main/java/assignment3/mst/model/ResultDTO.java
+++ src/main/java/assignment3/mst/model/ResultDTO.java
@@ -1,4 +1,42 @@
 package assignment3.mst.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultDTO {
+
+    public static class PerGraph {
+        public String graph;
+        public int vertices;
+        public int edges;
+        public AlgoResult prim;
+        public AlgoResult kruskal;
+        public boolean equalCost;
+        public boolean connected;
+    }
+
+    public static class AlgoResult {
+        public long mstCost;
+        public long timeMs;
+        public List<EdgeTriple> mstEdges = new ArrayList<>();
+        public Ops ops = new Ops();
+    }
+
+    public static class EdgeTriple {
+        public int u; public  int v; public int w;
+        public EdgeTriple() {}
+        public EdgeTriple(int u, int v, int w) {
+            this.u = u; this.v = v; this.w = w;
+        }
+    }
+
+    public static class Ops {
+        public long pqPushes; //Prim
+        public long pqPops;     //Prim
+        public long edgeConsidered;     //Prin
+        public long sortComparisons;    // optional, not measured precisely
+        public long edgeCheck;      // Kruslal - edges inspected
+        public long unions;     //Kruskal - succesful unions
+        public long finds;      //Kruskal - find operations
+    }
 }

8f58f60115a6c711cc907c62c829b8166368ea2c
inayatulla <241637@astanait.edu.kz>
2025-10-25 00:30:34 +0500

    add mst test class for the test

---
+++ src/test/java/MSTTests.java
@@ -0,0 +1,44 @@
+import assignment3.mst.algo.*;
+import assignment3.mst.io.JsonIO;
+import assignment3.mst.model.*;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MSTTests {
+    @Test
+    void smallGraph_correctness() throws Exception {
+        InputData data = JsonIO.readInput(Path.of("data/input_small.json"));
+        InputData.GraphItem gi = data.graphs.get(0);
+        Graph g = JsonIO.toGraph(gi);
+
+
+        ResultDTO.AlgoResult p = Prim.run(g);
+        ResultDTO.AlgoResult k = Kruskal.run(g);
+
+
+        assertEquals(p.mstCost, k.mstCost, "Prim vs Kruskal cost");
+
+
+        if (p.mstCost != Long.MAX_VALUE) {
+            assertEquals(g.V - 1, p.mstEdges.size(), "Prim edges count");
+            assertEquals(g.V - 1, k.mstEdges.size(), "Kruskal edges count");
+        }
+    }
+
+
+    @Test
+    void disconnectedGraph_detected() {
+        Graph g = new Graph(4, List.of(
+                new Edge(0,1,1),
+                new Edge(2,3,1)
+        ));
+        ResultDTO.AlgoResult p = Prim.run(g);
+        ResultDTO.AlgoResult k = Kruskal.run(g);
+        assertEquals(Long.MAX_VALUE, p.mstCost);
+        assertEquals(Long.MAX_VALUE, k.mstCost);
+    }
+}
